b3b3e092b5bc5a57f731cf075d8b4be6d58a236e
GURU SWAMY LANKA <LANKAGURUSWAMI@GMAIL.COM>
2026-08-12 00:21:49 +0530

    added ssl

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,14 +1,25 @@
 require('dotenv').config();
 
+const sslConfig =
+  process.env.DB_SSL === 'true'
+    ? {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      }
+    : {};
+
 module.exports = {
   development: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
+    port: Number(process.env.DB_PORT || 3306),
     dialect: process.env.DB_DIALECT || 'mysql',
     logging: false,
+    dialectOptions: sslConfig,
   },
 
   production: {
@@ -16,8 +27,9 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
+    port: Number(process.env.DB_PORT || 3306),
     dialect: process.env.DB_DIALECT || 'mysql',
     logging: false,
+    dialectOptions: sslConfig,
   },
 };
